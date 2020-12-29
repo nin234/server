d36acae183f324a5d8a2569abbb94865bdfad14d
Ninan Thomas <ninan@localhost.localdomain>
2020-12-29 07:00:14 -0500

    Distribution Manager initial checkin

---
+++ DistribMgr.cpp
@@ -0,0 +1,14 @@
+#include <DistribMgr.h>
+
+
+
+DistribMgr::DistribMgr()
+{
+
+}
+
+
+DistribMgr::~DistribMgr()
+{
+
+}
---
+++ DistribMgr.h
@@ -0,0 +1,13 @@
+#ifndef _DISTRIB_MGR_H_
+#define _DISTRIB_MGR_H_
+
+class DistribMgr
+{
+
+    public:
+
+        DistribMgr();
+        virtual ~DistribMgr();
+};
+
+#endif
