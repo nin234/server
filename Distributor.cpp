324d986eaa84dfc65ac62ac26e95d6324fc4b372
Ninan Thomas <ninan@localhost.localdomain>
2021-01-02 10:44:22 -0500

    Distribution related changes

--- Distributor.cpp
+++ Distributor.cpp
@@ -18,6 +18,7 @@ Distributor::distribute(LstObj *pLstObj)
     std::vector<std::string> shareIds;
 
     std::vector<std::string> lclShareIds;
+    std::vector<std::string> remoteShareIds;
 
     if (m_pTrnsl->getShareIds(pLstObj->getList(), shareIds))
     {
@@ -28,8 +29,14 @@ Distributor::distribute(LstObj *pLstObj)
             {
                 lclShareIds.push_back(shareIdStr);
             }
+            else
+            {
+                remoteShareIds.push_back(shareIdStr);
+            }
         }
     }
+    std::map<std::pair<std::string, int>, std::vector<std::string>> hostPortShareIds;
+    populateShareIdHostMap(pLstObj->getAppId(), hostPortShareIds, remoteShareIds);
     return lclShareIds;
 }
 
@@ -38,4 +45,20 @@ Distributor::setTrnsl(MessageTranslator *pTrnsl)
 {
 	m_pTrnsl.reset(pTrnsl);
 	return;
+}
+
+
+void 
+Distributor::populateShareIdHostMap(int appId, std::map<std::pair<std::string, int>, std::vector<std::string>>& hostPortShareIds, std::vector<std::string>& remoteShareIds)
+{
+    for (auto rShareIdStr : remoteShareIds)
+    {
+        long shareId = std::stol(rShareIdStr); 
+        std::pair<std::string, int> hostPort;
+        if (m_distribDAO.getNode(shareId, appId, hostPort))
+        {
+            hostPortShareIds[hostPort].push_back(rShareIdStr);
+        }
+        
+    }
 }
--- Distributor.h
+++ Distributor.h
@@ -5,11 +5,16 @@
 #include <string>
 #include <MessageObjs.h>
 #include <MessageTranslator.h>
+#include <map>
+#include <DistributedDAO.h>
 
 class Distributor
 {
 
         std::shared_ptr<MessageTranslator> m_pTrnsl;
+        void populateShareIdHostMap(int appId, std::map<std::pair<std::string, int>, std::vector<std::string>>& hostPortShareIds, std::vector<std::string>& remoteShareIds);
+        DistributedDAO m_distribDAO;
+
     public:
         Distributor();
         virtual ~Distributor();
