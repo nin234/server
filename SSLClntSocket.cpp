14af0d843c73cfcf84dbef57965abacdf5d4d8b2
Ninan Thomas <ninan@localhost.localdomain>
2021-01-03 15:36:39 -0500

    SSL client socket related changes

--- SSLClntSocket.cpp
+++ SSLClntSocket.cpp
@@ -1,9 +1,37 @@
 #include <SSLClntSocket.h>
+#include <iostream>
+#include <Util.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <fcntl.h>
+#include <unistd.h>
 
+
+thread_local SSL_CTX* SSLClntSocket::pCtx = NULL;
+
+thread_local std::mutex SSLClntSocket::ctx_init;
 
 SSLClntSocket::SSLClntSocket()
 {
 
+	if (!pCtx)
+	{
+		std::lock_guard<std::mutex> ctxLock(ctx_init);
+		if (pCtx)
+			return;
+		std::cout << Util::now() << "Initialising SSL client context " << __FILE__ << " " << __LINE__ << std::endl;
+		const SSL_METHOD *method;
+		method = TLS_client_method();
+		pCtx = SSL_CTX_new(method);
+		if (!pCtx)
+		{
+			std::cout <<Util::now() << "Failed to initialize ssl client context , exiting .... " << std::endl;
+			exit(0);
+		}
+
+
+    }
 }
 
 SSLClntSocket::~SSLClntSocket()
--- SSLClntSocket.h
+++ SSLClntSocket.h
@@ -1,8 +1,20 @@
 #ifndef _SSLCLNTSOCKET_H_
 #define _SSLCLNTSOCKET_H_
 
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+#include <mutex>
+#include <string>
+
 class SSLClntSocket
 {
+
+	thread_local static SSL_CTX *pCtx;
+	static thread_local std::mutex ctx_init;
+	int m_fd;
+	SSL *m_ssl;
+    std::string host;
+    int port;
 
     public:
         
